77cff1c99abff0c886bed1d1d2c269bba137888b
vasanth-raja <vasanthrajark@gmail.com>
2024-05-23 11:18:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero';
+import HomeCards from './components/HomeCards'
+import JobListings from './components/JobListings';
+const App = () => {
+  return (
+    <>
+      <Navbar />
+
+      {/* <!-- Hero --> */}
+      <Hero/>
+      {/* <!-- Developers and Employers --> */}
+      <HomeCards/>
+      
+
+      {/* <!-- Browse Jobs --> */}
+      <JobListings/>
+      
+
+      <section className="m-auto max-w-lg my-10 px-6">
+        <a
+          href="jobs.html"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          View All Jobs
+        </a>
+      </section>
+    </>
+  );
+}
+
+export default App
