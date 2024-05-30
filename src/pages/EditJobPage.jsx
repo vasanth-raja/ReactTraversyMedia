71120fe45121874438889481a343dea326838f58
vasanth-raja <vasanthrajark@gmail.com>
2024-05-30 15:52:05 +0530

    final commit

--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -1,22 +1,25 @@
-import React from "react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React ,{useState} from 'react'
 import {toast} from 'react-toastify'
-const AddJobPage = ({addJobSubmit}) => {
-    const [title,setTitle]=useState("");
-    const [type,setType]=useState("Full- time");
-    const [location,setLocation]=useState("");
-    const [description,setDescription]=useState("");
-    const [salary,setSalary]=useState("Under $50k");
-    const [companyName,setCompanyName]=useState("");
-    const [companyDescription,setcompanyDescription]=useState("");
-    const [contactEmail,setContactEmail]=useState("");
-    const [contactPhone,setContactPhone]=useState("");
+import { useLoaderData,useNavigate,useParams} from "react-router-dom";
+
+const EditJobPage = ({updateJobSubmit}) => {
+    const job =useLoaderData();
+    const [title,setTitle]=useState(job.title);
+    const [type,setType]=useState(job.type);
+    const [location,setLocation]=useState(job.location);
+    const [description,setDescription]=useState(job.description);
+    const [salary,setSalary]=useState(job.salary);
+    const [companyName,setCompanyName]=useState(job.company.name);
+    const [companyDescription,setcompanyDescription]=useState(job.company.description);
+    const [contactEmail,setContactEmail]=useState(job.company.contactEmail);
+    const [contactPhone,setContactPhone]=useState(job.company.contactPhone);
     const navigate= useNavigate();
+    const {id}=useParams();
     const submitForm=(e)=>{
         e.preventDefault();
         console.log(description)
-        const newJob={
+        const updatedJob={
+            id,
             title,
             type,
             location,
@@ -29,16 +32,16 @@ const AddJobPage = ({addJobSubmit}) => {
                 contactPhone
             }
         }
-        addJobSubmit(newJob)
-        toast.success('JOb added successfully')
-        return navigate('/jobs')
+        updateJobSubmit(updatedJob)
+        toast.success('Updated added successfully')
+        return navigate(`/jobs/${id}`)
     }
   return (
     <section className="bg-indigo-50">
       <div className="container m-auto max-w-2xl py-24">
         <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
           <form onSubmit={submitForm}>
-            <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
+            <h2 className="text-3xl text-center font-semibold mb-6">Update Job</h2>
 
             <div className="mb-4">
               <label htmlFor="type" className="block text-gray-700 font-bold mb-2">
@@ -208,14 +211,14 @@ const AddJobPage = ({addJobSubmit}) => {
                 className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
                 type="submit"
               >
-                Add Job
+                Update Job
               </button>
             </div>
           </form>
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default AddJobPage;
+export default EditJobPage
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,8 +1,17 @@
-import { useLoaderData, Link } from "react-router-dom";
+import { useLoaderData, Link,useNavigate } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
-const JobPage = () => {
-  const job = useLoaderData();
 
+import {toast} from 'react-toastify'
+const JobPage = ({deleteJob}) => {
+  const navigate=useNavigate();
+  const job = useLoaderData();
+  const onDeleteClick=(jobId)=>{
+    const confirm =window.confirm('Are you sure you want to delete this listing')
+    if(!confirm) return
+    deleteJob(jobId)
+    toast.success('Job deleted succesfully')
+    navigate('/jobs')
+  }
   return (
     <>
       <section>
@@ -71,12 +80,12 @@ const JobPage = () => {
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                 <Link
-                  to={`/jobs/edit/${job.id}`}
+                  to={`/edit-job/${job.id}`}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Edit Job
                 </Link>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <button onClick={()=>{onDeleteClick(job.id)}} className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
                   Delete Job
                 </button>
               </div>
